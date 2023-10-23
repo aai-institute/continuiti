ff8745bbf01a6321f2900c331bc3b8db28f05b66
Samuel Burbulla <s.burbulla@appliedai-institute.de>
2023-10-23 18:59:08 +0200

    Improve plots.

--- plotting.py
+++ plotting.py
@@ -6,12 +6,12 @@ def plot_observation(observation, ax=plt.gca()):
     """Plot observation"""
     x = [s.x for s in observation.sensors]
     u = [s.u for s in observation.sensors]
-    ax.plot(x, u, 'ko')
+    ax.plot(x, u, 'k.')
 
 
 def plot_evaluation(model, dataset, observation, ax=plt.gca()):
     """Plot evaluation"""
-    n = 100
+    n = 200
     x = np.linspace(-1, 1, n)
     u = np.zeros_like(x)
     for i in range(n):
