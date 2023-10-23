6b45b2a76e2d1323367f96826c4577adcfd980c5
Samuel Burbulla <s.burbulla@appliedai-institute.de>
2023-10-23 20:16:32 +0200

    Add 2d plotting.

--- plotting.py
+++ plotting.py
@@ -6,16 +6,44 @@ def plot_observation(observation, ax=plt.gca()):
     """Plot observation"""
     x = [s.x for s in observation.sensors]
     u = [s.u for s in observation.sensors]
-    ax.plot(x, u, 'k.')
+
+    dim = x[0].shape[0]
+    if dim == 1:
+        ax.plot(x, u, 'k.')
+    if dim == 2:
+        xx, yy = [x[0] for x in x], [x[1] for x in x]
+        ax.scatter(xx, yy, s=20, c=u, cmap='jet')
 
 
 def plot_evaluation(model, dataset, observation, ax=plt.gca()):
     """Plot evaluation"""
-    n = 200
-    x = np.linspace(-1, 1, n)
-    u = np.zeros_like(x)
-    for i in range(n):
-        obs_pos = dataset.flatten(observation, np.array([x[i]]))
-        obs_pos = obs_pos.reshape(1, -1)
-        u[i] = model(obs_pos)
-    ax.plot(x, u, 'k-')
+    dim = dataset.coordinate_dim
+
+    if dim == 1:
+        n = 200
+        x = np.linspace(-1, 1, n)
+        u = np.zeros_like(x)
+        for i in range(n):
+            obs_pos = dataset.flatten(observation, np.array([x[i]]))
+            obs_pos = obs_pos.reshape(1, -1)
+            u[i] = model(obs_pos)
+        ax.plot(x, u, 'k-')
+
+    if dim == 2:
+        n = 32
+        x = np.linspace(-1, 1, n)
+        y = np.linspace(-1, 1, n)
+        xx, yy = np.meshgrid(x, y)
+        u = np.zeros_like(xx)
+        for i in range(n):
+            for j in range(n):
+                print(f"\rPlotting... {i*j}/{n*n}", end="")
+                obs_pos = dataset.flatten(
+                    observation, 
+                    np.array([xx[i, j], yy[i, j]])
+                )
+                obs_pos = obs_pos.reshape(1, -1)
+                u[i, j] = model(obs_pos)
+        cb = ax.contourf(xx, yy, u, cmap='jet')
+        ax.set_aspect('equal')
+        plt.colorbar(cb, ax=ax)
