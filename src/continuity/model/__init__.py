a8a13f910eb6caa286e1e2c913934d9adaab0479
Samuel Burbulla <s.burbulla@appliedai-institute.de>
2023-11-20 18:47:02 +0200

    Disable mps on github.

--- src/continuity/model/__init__.py
+++ src/continuity/model/__init__.py
@@ -1,9 +1,13 @@
+import os
 import torch
 
 # Device
 device = torch.device("cpu")
 
-if torch.backends.mps.is_available():
-    device = torch.device("mps")
-elif torch.cuda.is_available():
-    device = torch.device("cuda")
+# If we are not running on GitHub Actions, we choose MPS or CUDA
+if os.getenv("GITHUB_ACTIONS") != "true":
+    if torch.backends.mps.is_available():
+        device = torch.device("mps")
+
+    elif torch.cuda.is_available():
+        device = torch.device("cuda")
