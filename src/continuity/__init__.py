49309ec96aa233bdb32d29bd515f80ebfdfc1b3b
Samuel Burbulla <s.burbulla@appliedai-institute.de>
2024-01-23 16:08:54 +0100

    Add cards to API landing page.

--- src/continuity/__init__.py
+++ src/continuity/__init__.py
@@ -1,1 +1,16 @@
-"""The Continuity package."""
+"""
+**Continuity** is a Python package for machine learning on function operators.
+
+::cards:: cols=2
+
+- title: Data
+  content: Data set implementations
+  url: data/index.md
+
+- title: Operators
+  content: Neural operator implementations
+  url: operators/index.md
+
+::/cards::
+
+"""
