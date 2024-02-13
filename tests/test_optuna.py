661438c06a50163f3a3145728e2dd4ad3690e2ca
Jakob <jakob@winter-wind.de>
2024-02-13 11:06:56 +0100

    adjust tests to new dataset interface

--- tests/test_optuna.py
+++ tests/test_optuna.py
@@ -3,7 +3,6 @@ from continuity.benchmarks.sine import SineBenchmark
 from continuity.callbacks import OptunaCallback
 from continuity.data import split, dataset_loss
 from continuity.operators import DeepONet
-from torch.utils.data import DataLoader
 import optuna
 
 # Set random seed
@@ -22,14 +21,10 @@ def test_optuna():
 
         # Train/val split
         train_dataset, val_dataset = split(benchmark.train_dataset, 0.9)
-        train_dataloader = DataLoader(train_dataset)
-        val_dataloader = DataLoader(val_dataset)
 
         # Operator
         operator = DeepONet(
-            benchmark.dataset.num_sensors,
-            benchmark.dataset.coordinate_dim,
-            benchmark.dataset.num_channels,
+            benchmark.dataset.shape,
             trunk_width=trunk_width,
             trunk_depth=trunk_depth,
         )
@@ -39,10 +34,10 @@ def test_optuna():
 
         operator.compile(optimizer, verbose=False)
         operator.fit(
-            train_dataloader, epochs=num_epochs, callbacks=[OptunaCallback(trial)]
+            train_dataset, epochs=num_epochs, callbacks=[OptunaCallback(trial)]
         )
 
-        loss_val = dataset_loss(val_dataloader, operator, benchmark.metric())
+        loss_val = dataset_loss(val_dataset, operator, benchmark.metric())
         print(f"loss/val: {loss_val:.4e}")
 
         return loss_val
