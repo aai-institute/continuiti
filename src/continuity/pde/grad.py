478e1006131944f4a683ef1fe8593d16a6a4696a
Samuel Burbulla <s.burbulla@appliedai-institute.de>
2024-02-14 14:24:49 +0100

    Add div operator.

--- src/continuity/pde/grad.py
+++ src/continuity/pde/grad.py
@@ -60,3 +60,49 @@ def grad(u: Callable[[Tensor], Tensor]) -> Callable[[Tensor], Tensor]:
         Function that computes the gradient of the input function.
     """
     return lambda x: Grad()(x, u(x))
+
+
+class Div(Operator):
+    """Divergence operator.
+
+    The divergence is a function operator that maps a function to its divergence.
+    """
+
+    def forward(self, x: Tensor, u: Tensor, y: Optional[Tensor] = None) -> Tensor:
+        """Forward pass through the operator.
+
+        Args:
+            x: Tensor of sensor positions of shape (batch_size, num_sensors, input_coordinate_dim)
+            u: Tensor of sensor values of shape (batch_size, num_sensors, input_channels)
+            y: Tensor of evaluation positions of shape (batch_size, y_size, output_coordinate_dim)
+
+        Returns:
+            Tensor of evaluations of the mapped function of shape (batch_size, y_size, output_channels)
+        """
+        if y is not None:
+            assert torch.equal(x, y), "x and y must be equal for divergence operator"
+
+        assert x.requires_grad, "x must require gradients for divergence operator"
+
+        # Compute gradients
+        gradients = torch.autograd.grad(
+            u,
+            x,
+            grad_outputs=torch.ones_like(u),
+            create_graph=True,
+            retain_graph=True,
+        )[0]
+
+        return torch.sum(gradients, dim=-1, keepdim=True)
+
+
+def div(u: Callable[[Tensor], Tensor]) -> Callable[[Tensor], Tensor]:
+    """Compute the divergence of a function.
+
+    Args:
+        u: Function to compute the divergence of.
+
+    Returns:
+        Function that computes the divergence of the input function.
+    """
+    return lambda x: Div()(x, u(x))
--- tests/test_grad.py
+++ tests/test_grad.py
@@ -1,5 +1,5 @@
 import torch
-from continuity.pde.grad import grad, Grad
+from continuity.pde.grad import grad, Grad, div, Div
 
 # Set random seed
 torch.manual_seed(0)
@@ -15,10 +15,7 @@ def test_grad():
         return torch.stack([2 * x[:, 0], 3 * x[:, 1] ** 2], dim=1)
 
     x = torch.rand(100, 2).requires_grad_(True)
-    print(f" x.shape: {x.shape}")
-
     u = f(x)
-    print(f" u.shape: {u.shape}")
 
     # Test gradient of function
     gf = grad(f)
@@ -26,9 +23,30 @@ def test_grad():
 
     # Test gradient operator
     du = Grad()(x, u, x)
-    print(f"du.shape: {du.shape}")
     assert torch.norm(du - df(x)) < 1e-6
+
+
+def test_div():
+    # f(x) = x_0^2 + x_1^3
+    def f(x):
+        return (x[:, 0] ** 2 + x[:, 1] ** 3).unsqueeze(1)
+
+    # div_f(x) = 2 * x_0 + 3 * x_1^2
+    def div_f(x):
+        return (2 * x[:, 0] + 3 * x[:, 1] ** 2).unsqueeze(1)
+
+    x = torch.rand(100, 2).requires_grad_(True)
+    u = f(x)
+
+    # Test divergence of function
+    df = div(f)
+    assert torch.norm(df(x) - div_f(x)) < 1e-6
+
+    # Test divergence operator
+    div_u = Div()(x, u, x)
+    assert torch.norm(div_u - div_f(x)) < 1e-6
 
 
 if __name__ == "__main__":
     test_grad()
+    test_div()
