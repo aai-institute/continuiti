d3acddaae067a4524fc24949bb06714f2bce507f
Samuel Burbulla <s.burbulla@appliedai-institute.de>
2024-02-22 12:40:35 +0100

    Use device from data.

--- tests/trainer/test_trainer.py
+++ tests/trainer/test_trainer.py
@@ -1,7 +1,7 @@
 import torch
 from torch.utils.data import DataLoader
 from continuity.operators import DeepONet
-from continuity.data import Sine
+from continuity.data import device, Sine
 from continuity.trainer import Trainer
 
 torch.manual_seed(0)
@@ -12,21 +12,15 @@ def test_trainer():
     data_loader = DataLoader(dataset)
     operator = DeepONet(dataset.shapes)
 
-    device = "cpu"
-    if torch.cuda.is_available():
-        device = "cuda"
-    if torch.backends.mps.is_available():
-        device = "mps"
     print(f"Using device: {device}")
-
     trainer = Trainer(operator, device=device)
-    trainer.fit(data_loader, epochs=10)
+    trainer.fit(data_loader, epochs=2)
 
     # Make sure we can use operator output on cpu again
     x, u, y, v = next(iter(data_loader))
     v_pred = operator(x, u, y)
     mse = ((v_pred - v.to("cpu")) ** 2).mean()
-    print(mse.item())
+    print(f"mse = {mse.item():.3g}")
 
 
 if __name__ == "__main__":
