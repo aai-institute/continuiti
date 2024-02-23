593ce5e37e5407b8344c474d3ec03c99204fb8ca
JakobEliasWagner <42122260+JakobEliasWagner@users.noreply.github.com>
2024-02-23 13:43:25 +0100

    Feature: Normalize (#51)
    Add z-normalization class.
    
    ---------
    
    Co-authored-by: Samuel Burbulla <s.burbulla@appliedai-institute.de>

--- src/continuity/transforms/__init__.py
+++ src/continuity/transforms/__init__.py
@@ -6,8 +6,10 @@ Data transformations in Continuity.
 
 from .transform import Transform
 from .compose import Compose
+from .scaling import Normalize
 
 __all__ = [
     "Transform",
     "Compose",
+    "Normalize",
 ]
---
+++ tests/transforms/test_scaling.py
@@ -0,0 +1,96 @@
+import pytest
+import torch
+
+from continuity.transforms import Normalize
+
+# Set random seed for reproducibility
+torch.manual_seed(0)
+
+
+@pytest.fixture(scope="module")
+def random_normalization_set():
+    mean = torch.rand((1, 1, 15, 7))
+    std = torch.rand((1, 1, 15, 7))
+    tf = Normalize(mean=mean, std=std)
+    t = torch.rand((100, 3, 15, 7))
+    return tf, t
+
+
+def test_normalization_zero():
+    mean = torch.zeros((1, 3))
+    std = torch.rand((1, 3))
+
+    tf = Normalize(mean=mean, std=std)
+
+    assert torch.allclose(tf(torch.zeros(100, 3)), torch.zeros(100, 3))
+
+
+def test_normalization_unchanged():
+    mean = torch.zeros((1, 3))
+    std = torch.ones((1, 3))
+    tf = Normalize(mean=mean, std=std)
+
+    t = torch.rand((100, 3))
+
+    assert torch.allclose(tf(t), t)
+
+
+def test_normalization_mean():
+    mean = torch.ones((1, 3))
+    std = torch.ones((1, 3))
+    tf = Normalize(mean=mean, std=std)
+
+    t = torch.rand((100, 3))
+    assert torch.allclose(tf(t), t - 1.0)
+
+
+def test_normalization_std():
+    factor = 2.0
+    mean = torch.zeros((1, 3))
+    std = factor * torch.ones((1, 3))
+    tf = Normalize(mean=mean, std=std)
+
+    t = torch.rand((100, 3))
+    assert torch.allclose(tf(t), t / factor)
+
+
+def test_normalization_correct():
+    t = torch.tensor([2.0, 4.0, 4.0, 4.0, 5.0, 5.0, 7.0, 9.0]).to(
+        torch.get_default_dtype()
+    )
+    mean = torch.mean(t)
+    std = torch.std(
+        t, correction=0
+    )  # correction as otherwise it defaults to Bessel's correction
+    assert mean == 5.0
+    assert std == 2.0
+    tf = Normalize(mean, std)
+
+    assert torch.allclose(tf(t), (t - 5.0) / 2.0)
+
+
+def test_normalization_singular():
+    mean = torch.zeros((1, 3))
+    std = torch.zeros((1, 3))
+    tf = Normalize(mean=mean, std=std)
+
+    t = torch.rand((100, 3))
+    tf_t = tf(t)
+
+    assert not torch.any(torch.isnan(tf_t))
+    assert not torch.any(torch.isinf(tf_t))
+
+
+def test_normalization_dimensions(random_normalization_set):
+    tf, t = random_normalization_set
+    assert tf(t).shape == t.shape
+
+
+def test_normalization_undo(random_normalization_set):
+    tf, t = random_normalization_set
+
+    t_normalized = tf(t)
+    t2 = tf.undo(t_normalized)
+
+    # higher tolerance required because of numerical accuracy of these operations
+    assert torch.allclose(t, t2, atol=1e-7)
