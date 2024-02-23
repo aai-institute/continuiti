fix numerical stability implementation

src/continuity/transforms/scaling.py
@@ -16,33 +16,26 @@ class Normalize(Transform):
         epsilon: Value to prevent divide by zero.
     """
 
-    def __init__(
-        self,
-        mean: torch.Tensor,
-        std: torch.Tensor,
-        epsilon: float = None,
-    ):
+    def __init__(self, mean: torch.Tensor, std: torch.Tensor):
         """
 
         Args:
             mean: mean used to scale tensors.
             std: standard deviation used to scale tensors in forward.
-            epsilon: Value for numerical stability (to prevent divide by zero).
         """
-        assert torch.all(torch.greater_equal(std, torch.zeros(std.shape)))
         super().__init__()
         self.mean = mean
-        if torch.allclose(std, torch.zeros(std.shape)):
+        if not std.all():
+            # some value is zero
+            epsilon = torch.finfo(torch.get_default_dtype()).tiny
             warnings.warn(
-                "Z-normalization with standard deviation 0! "
-                "The feature vector does not have any discriminative power!",
+                "Normalization with standard deviation 0. "
+                "Some or all features do not have any discriminative power! "
+                f"Introducing a epsilon={epsilon} to account for numerical stability.",
                 stacklevel=2,
             )
+            std[std == 0] = epsilon
         self.std = std
-        if epsilon is None:
-            self.epsilon = torch.finfo(torch.get_default_dtype()).tiny
-        else:
-            self.epsilon = epsilon
 
     def forward(self, tensor: torch.Tensor) -> torch.Tensor:
         r"""Applies normalization to the input tensor.
@@ -53,7 +46,7 @@ class Normalize(Transform):
         Returns:
             normalized tensor.
         """
-        return (tensor - self.mean) / (self.std + self.epsilon)
+        return (tensor - self.mean) / self.std
 
     def undo(self, tensor: torch.Tensor) -> torch.Tensor:
         r"""Reverse the normalization.
@@ -64,4 +57,4 @@ class Normalize(Transform):
         Returns:
             tensor with normalization undone.
         """
-        return tensor * (self.std + self.epsilon) + self.mean
+        return tensor * self.std + self.mean

tests/transforms/test_scaling.py
@@ -69,13 +69,13 @@ def test_normalization_correct():
 def test_normalization_singular():
     mean = torch.zeros((1, 3))
     std = torch.zeros((1, 3))
-    with pytest.warns(UserWarning) as record:
-        tf = Normalize(mean=mean, std=std)
-    assert len(record) == 1
+    tf = Normalize(mean=mean, std=std)
 
     t = torch.rand((100, 3))
+    tf_t = tf(t)
 
-    assert not torch.any(torch.isnan(tf(t)))
+    assert not torch.any(torch.isnan(tf_t))
+    assert not torch.any(torch.isinf(tf_t))
 
 
 def test_normalization_dimensions(random_normalization_set):