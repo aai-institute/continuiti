70cbdb799fbf74580cbcb8f6abba8e8973bde82d
Samuel Burbulla <s.burbulla@appliedai-institute.de>
2024-02-26 10:52:02 +0000

    Implement parallel trainer.

--- tests/trainer/test_trainer.py
+++ tests/trainer/test_trainer.py
@@ -1,27 +1,54 @@
 import torch
+import pytest
 from torch.utils.data import DataLoader
 from continuity.operators import DeepONet
 from continuity.data.sine import Sine
 from continuity.trainer import Trainer
+from continuity.trainer import Trainer
+import torch.distributed as dist
 
 torch.manual_seed(0)
 
 
-def test_trainer():
+def train(rank: int = "cpu", verbose: bool = True):
     dataset = Sine(num_sensors=32, size=16)
     data_loader = DataLoader(dataset)
     operator = DeepONet(dataset.shapes)
 
-    trainer = Trainer(operator)
-    print(f"Using device: {trainer.device}")
+    optimizer = torch.optim.Adam(operator.parameters(), lr=1e-2)
+    trainer = Trainer(operator, optimizer, device=rank, verbose=verbose)
     trainer.fit(data_loader, epochs=2)
 
     # Make sure we can use operator output on cpu again
     x, u, y, v = next(iter(data_loader))
     v_pred = operator(x, u, y)
     mse = ((v_pred - v.to("cpu")) ** 2).mean()
-    print(f"mse = {mse.item():.3g}")
+    if verbose:
+        print(f"mse = {mse.item():.3g}")
+
+
+@pytest.mark.slow
+def test_trainer():
+    train()
+
+
+# Use ./run_parallel.sh to run test with CUDA
+def train_parallel():
+    if torch.cuda.device_count() == 0:
+        print("Skipping CUDA tests because no GPU is available.")
+        return
+
+    dist.init_process_group("nccl")
+    rank = dist.get_rank()
+
+    if rank == 0:
+        print(f" == Number of GPUs: {dist.get_world_size()}")
+
+    verbose = rank == 0
+    train(rank, verbose=verbose)
+
+    dist.destroy_process_group()
 
 
 if __name__ == "__main__":
-    test_trainer()
+    train_parallel()
