1c181c51e41da7e472d4b7d6602eaee1dd873cfd
Jakob <jakob@winter-wind.de>
2024-02-27 15:09:56 +0100

    add set seed fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,13 @@
+import pytest
+import random
+import torch
+
 pytest_plugins = [
     "tests.transforms.fixtures",
 ]
+
+
+@pytest.fixture(autouse=True)
+def set_random_seed():
+    random.seed(0)
+    torch.manual_seed(0)
