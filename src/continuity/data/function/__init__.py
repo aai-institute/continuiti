fca844ee60e9ddeddf4e99050ad14ab98274c113
Jakob <jakob@winter-wind.de>
2024-02-28 10:17:25 +0100

    add function space and sampled function space skeleton

---
+++ src/continuity/data/function/__init__.py
@@ -0,0 +1,9 @@
+from .function import Function, ParameterizedFunction
+from .function_space import FunctionSpace, SampledFunctionSpace
+
+__all__ = [
+    "Function",
+    "ParameterizedFunction",
+    "FunctionSpace",
+    "SampledFunctionSpace",
+]
