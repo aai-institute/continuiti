8002a7fb0453d01779d4b923494de29bcfaf0793
Jakob <jakob@winter-wind.de>
2024-03-12 15:00:28 +0100

    cleanup

--- src/continuity/data/function/function_set.py
+++ src/continuity/data/function/function_set.py
@@ -5,7 +5,7 @@ Function set.
 """
 
 import torch
-from typing import Callable, List, Union
+from typing import Callable, List
 from .function import Function
 
 
@@ -16,7 +16,7 @@ class FunctionSet:
     functions F(X, Y) that map from X to Y and is closed with respect to addition
     $(f+g)(x):X \rightarrow Y, x\mapsto f(x)+g(x)$ and scalar multiplication $(cf)(x) \rightarrow Y, x\mapsto cf(x)$.
     In practice, we consider subsets of function spaces (where in general, these properties are not respected) and,
-    therefore, this class implements a (parametrized) FunctionSet.
+    therefore, this class implements a (parametrized) function set.
 
     Args:
         parameterized_mapping: A two level nested callable that takes a single parameter in the outer callable as
@@ -24,7 +24,7 @@ class FunctionSet:
 
     Example:
         ```python
-        p_sine = FunctionSet(lambda a: lambda x: a * torch.sin(x))
+        p_sine = FunctionSet(lambda a: Function(lambda x: a * torch.sin(x)))
         param = torch.arange(5)
         for func in sine(param):
             print(type(func))
@@ -43,9 +43,7 @@ class FunctionSet:
     def __init__(self, parameterized_mapping: Callable):
         self.parameterized_mapping = parameterized_mapping
 
-    def __call__(
-        self, parameters: Union[torch.Tensor, List[torch.Tensor]]
-    ) -> List[Function]:
+    def __call__(self, parameters: torch.Tensor) -> List[Function]:
         """Evaluates the function set for a specific discrete instance of parameters.
 
         Args:
