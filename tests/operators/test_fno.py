5d480a80c448c6df3c04230cd8ef90492c624236
Michel Luchmann <m.luchmann@appliedai.de>
2024-03-13 13:56:23 +0100

    Update FourierLayer tests

--- tests/operators/test_fno.py
+++ tests/operators/test_fno.py
@@ -62,5 +62,149 @@ def test_fno():
     assert MSELoss()(operator, x, u, y, v) < 1e-12
 
 
+def test_zero_padding():
+    dataset = get_dataset()
+    operator = FourierLayer(dataset.shapes)
+
+    #### test behaviour for odd number of frequencies
+    # input tensor in 'standard order'
+    x = torch.tensor(
+        [
+            [0.0000, 1.0000, 2.0000],
+            [0.1000, 1.1000, 2.1000],
+            [0.2000, 1.2000, 2.2000],
+            [-0.2000, 0.8000, 1.8000],
+            [-0.1000, 0.9000, 1.9000],
+        ]
+    )
+
+    output = torch.tensor(
+        [
+            [0.0000, 1.0000, 2.0000, 0.0000],
+            [0.1000, 1.1000, 2.1000, 0.0000],
+            [0.2000, 1.2000, 2.2000, 0.0000],
+            [0.0000, 0.0000, 0.0000, 0.0000],
+            [0.0000, 0.0000, 0.0000, 0.0000],
+            [-0.2000, 0.8000, 1.8000, 0.0000],
+            [-0.1000, 0.9000, 1.9000, 0.0000],
+        ]
+    )
+
+    # test if zero padding works well together with `fftshift` method
+    # if we include zeros at the wrong position, the 'standard order' will be destroyed
+    x_shifted = torch.fft.fftshift(x, dim=(0,))
+    x_zero_padded = operator.zero_padding(
+        x_shifted, target_shape=output.shape, dim=(0, 1)
+    )
+    x_zero_padded = torch.fft.ifftshift(x_zero_padded, dim=(0,))
+
+    assert torch.all(output == x_zero_padded).item()
+
+    #### test behaviour for even number of frequencies
+    # input tensor in 'standard order'
+    x = torch.tensor(
+        [
+            [0.0000, 1.0000, -2.0000],
+            [0.1000, 1.1000, -1.9000],
+            [-0.2000, 0.8000, -2.2000],
+            [-0.1000, 0.9000, -2.1000],
+        ]
+    )
+
+    output = torch.tensor(
+        [
+            [0.0000, 1.0000, -2.0000, 0.0000],
+            [0.1000, 1.1000, -1.9000, 0.0000],
+            [0.0000, 0.0000, 0.0000, 0.0000],
+            [0.0000, 0.0000, 0.0000, 0.0000],
+            [0.0000, 0.0000, 0.0000, 0.0000],
+            [-0.2000, 0.8000, -2.2000, 0.0000],
+            [-0.1000, 0.9000, -2.1000, 0.0000],
+        ]
+    )
+
+    # test if zero padding works well together with `fftshift` method
+    # if we include zeros at the wrong position, the 'standard order' will be destroyed
+    x_shifted = torch.fft.fftshift(x, dim=(0,))
+    x_zero_padded = operator.zero_padding(
+        x_shifted, target_shape=output.shape, dim=(0, 1)
+    )
+    x_zero_padded = torch.fft.ifftshift(x_zero_padded, dim=(0,))
+
+    assert torch.all(output == x_zero_padded).item()
+
+
+def test_remove_large_frequencies():
+    dataset = get_dataset()
+    operator = FourierLayer(dataset.shapes)
+
+    #### test behaviour for odd number of frequencies
+    # input tensor in 'standard order'
+    # frequencies to be removed are marked with zeros
+    x = torch.tensor(
+        [
+            [0.0000, 1.0000, 2.0000, 0.0000],
+            [0.1000, 1.1000, 2.1000, 0.0000],
+            [0.2000, 1.2000, 2.2000, 0.0000],
+            [0.0000, 0.0000, 0.0000, 0.0000],
+            [0.0000, 0.0000, 0.0000, 0.0000],
+            [-0.2000, 0.8000, 1.8000, 0.0000],
+            [-0.1000, 0.9000, 1.9000, 0.0000],
+        ]
+    )
+
+    output = torch.tensor(
+        [
+            [0.0000, 1.0000, 2.0000],
+            [0.1000, 1.1000, 2.1000],
+            [0.2000, 1.2000, 2.2000],
+            [-0.2000, 0.8000, 1.8000],
+            [-0.1000, 0.9000, 1.9000],
+        ]
+    )
+
+    # test if removing frequencies works well together with `fftshift` method
+    x_shifted = torch.fft.fftshift(x, dim=(0,))
+    x_zero_padded = operator.remove_large_frequencies(
+        x_shifted, target_shape=output.shape, dim=(0, 1)
+    )
+    x_zero_padded = torch.fft.ifftshift(x_zero_padded, dim=(0,))
+
+    assert torch.all(output == x_zero_padded).item()
+
+    #### test behaviour for even number of frequencies
+    # input tensor in 'standard order'
+    # frequencies to be removed are marked with zeros
+    x = torch.tensor(
+        [
+            [0.0000, 1.0000, -2.0000, 0.0000],
+            [0.1000, 1.1000, -1.9000, 0.0000],
+            [0.0000, 0.0000, 0.0000, 0.0000],
+            [0.0000, 0.0000, 0.0000, 0.0000],
+            [0.0000, 0.0000, 0.0000, 0.0000],
+            [-0.2000, 0.8000, -2.2000, 0.0000],
+            [-0.1000, 0.9000, -2.1000, 0.0000],
+        ]
+    )
+
+    output = torch.tensor(
+        [
+            [0.0000, 1.0000, -2.0000],
+            [0.1000, 1.1000, -1.9000],
+            [-0.2000, 0.8000, -2.2000],
+            [-0.1000, 0.9000, -2.1000],
+        ]
+    )
+
+    # test if zero padding works well together with `fftshift` method
+    x_shifted = torch.fft.fftshift(x, dim=(0,))
+    x_zero_padded = operator.remove_large_frequencies(
+        x_shifted, target_shape=output.shape, dim=(0, 1)
+    )
+    x_zero_padded = torch.fft.ifftshift(x_zero_padded, dim=(0,))
+
+    assert torch.all(output == x_zero_padded).item()
+
+
 if __name__ == "__main__":
-    test_fourier1d()
+    test_zero_padding()
