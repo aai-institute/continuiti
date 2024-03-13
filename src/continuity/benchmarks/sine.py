1fbfe8f6282b5ae31eb38eac8045588e492bd06a
Jakob <jakob@winter-wind.de>
2024-03-13 14:27:01 +0100

    decouple sine benchmark from Sine dataset class

--- src/continuity/benchmarks/sine.py
+++ src/continuity/benchmarks/sine.py
@@ -3,16 +3,90 @@
 
 Sine benchmark.
 """
-
+import torch
+from continuity.data.function import FunctionOperatorDataset, FunctionSet, Function
 from continuity.benchmarks import Benchmark
-from continuity.data.sine import Sine
+from continuity.discrete import RegularGridSampler
 
-"""Sine benchmark.
 
-A sine wave data set containing 100 samples with 32 sensors,
-split randomly into 90% training and 10% test data.
-"""
-sine_benchmark = Benchmark(
-    train_dataset=Sine(num_sensors=32, size=90),
-    test_dataset=Sine(num_sensors=32, size=10),
+def create_sine_dataset(
+    n_observations: int, n_sensors: int, n_evaluations: int
+) -> FunctionOperatorDataset:
+    r"""Creates a sine dataset.
+
+    A sine dataset is described by the set of trigonometric functions $f(x)_k = \sin(kx)$, with $x\in[-1, 1]$ and
+    $k\in[\pi, 2\pi]$. The input space is mapped to the output space with the identity operator (input and output set
+    are the same). Both the parameter space, the domain, and the co-domain are sampled on a regular grid.
+
+    Args:
+        n_observations: number of observations.
+        n_sensors: number of sensors.
+        n_evaluations: number of evaluations.
+
+    Returns:
+        Dataset of parameterized sine functions mapped by the identity operator.
+    """
+    sine_set = FunctionSet(lambda k: Function(lambda x: torch.sin(k * x)))
+
+    x_sampler = y_sampler = RegularGridSampler([-1.0], [1.0])
+    parameter_sampler = RegularGridSampler([torch.pi], [2 * torch.pi])
+
+    sine_dataset = FunctionOperatorDataset(
+        input_function_set=sine_set,
+        x_sampler=x_sampler,
+        n_sensors=n_sensors,
+        output_function_set=sine_set,
+        y_sampler=y_sampler,
+        n_evaluations=n_evaluations,
+        parameter_sampler=parameter_sampler,
+        n_observations=n_observations,
+    )
+
+    return sine_dataset
+
+
+def create_sine_benchmark(
+    n_observations: int = 100,
+    n_sensors: int = 32,
+    n_evaluations: int = None,
+    n_test_observations: int = None,
+) -> Benchmark:
+    r"""Creates a sine benchmark.
+
+    A sine benchmark is sampled on a regular grid, both in space and parameters. The input function set is mapped onto
+    the output function set with the identity operator (input and output function sets are the same). The function sets
+    are created with the create_sine_dataset function.
+
+    Args:
+        n_observations: number of observations in the train dataset.
+        n_sensors: number of sensors.
+        n_evaluations: number of evaluation coordinates, defaults to n_sensors.
+        n_test_observations: number of observations in the test dataset, defaults to int(n_observations / 10).
+
+    Returns:
+        Sine benchmark.
+    """
+    if n_evaluations is None:
+        n_evaluations = n_sensors
+    if n_test_observations is None:
+        n_test_observations = n_observations // 10
+
+    benchmark = Benchmark(
+        train_dataset=create_sine_dataset(
+            n_observations=n_observations,
+            n_sensors=n_sensors,
+            n_evaluations=n_evaluations,
+        ),
+        test_dataset=create_sine_dataset(
+            n_observations=n_test_observations,
+            n_sensors=n_sensors,
+            n_evaluations=n_evaluations,
+        ),
+    )
+
+    return benchmark
+
+
+sine_benchmark = create_sine_benchmark(
+    n_observations=90, n_sensors=32, n_evaluations=32, n_test_observations=10
 )
