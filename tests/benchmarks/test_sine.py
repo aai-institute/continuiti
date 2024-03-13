0bfc7a3ec483344449a655df6181f2910d2530b9
Jakob <jakob@winter-wind.de>
2024-03-13 14:44:35 +0100

    add tests

---
+++ tests/benchmarks/test_sine.py
@@ -0,0 +1,16 @@
+from continuity.benchmarks import Benchmark
+from continuity.benchmarks.sine import (
+    create_sine_dataset,
+    create_sine_benchmark,
+    sine_benchmark,
+)
+from continuity.data import OperatorDataset
+
+
+def test_return_type_correct():
+    assert isinstance(create_sine_dataset(2, 3, 5), OperatorDataset)
+    assert isinstance(create_sine_benchmark(1, 2, 3, 5), Benchmark)
+
+
+def test_can_initialize_default():
+    assert isinstance(sine_benchmark, Benchmark)
