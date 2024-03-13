d7c9dc728f988724418ad49138002ca2bbcf7e58
Jakob <jakob@winter-wind.de>
2024-03-13 13:39:07 +0100

    replace metrics by using loss instead

--- src/continuity/benchmarks/benchmark.py
+++ src/continuity/benchmarks/benchmark.py
@@ -7,26 +7,22 @@ Benchmark base class.
 from typing import List
 from dataclasses import dataclass, field
 from continuity.data import OperatorDataset
-from continuity.benchmarks.metrics import (
-    Metric,
-    L1Metric,
-    MSEMetric,
-    NumberOfParameters,
-    SpeedOfEvaluation,
-)
+from continuity.operators.losses import Loss, MSELoss
 
 
 @dataclass
 class Benchmark:
-    """Benchmark class."""
+    """Benchmark class.
+
+    A Benchmark object encapsulates two distinct datasets: a train and a test dataset. The training dataset is used to
+    train an operator to fit the dataset. The test dataset, in contrast, is utilized solely for evaluating the
+    performance. The evaluation is done by measuring the loss on the test set.
+    """
 
     train_dataset: OperatorDataset
     test_dataset: OperatorDataset
-    metrics: List[Metric] = field(
+    losses: List[Loss] = field(
         default_factory=lambda: [
-            L1Metric(),
-            MSEMetric(),
-            NumberOfParameters(),
-            SpeedOfEvaluation(),
+            MSELoss(),
         ]
     )
