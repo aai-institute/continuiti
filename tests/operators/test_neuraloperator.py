24605b25f68f5aa8e8803a2b87cc7990ba1ce33a
Samuel Burbulla <s.burbulla@appliedai-institute.de>
2024-03-14 17:27:33 +0100

    Generalize NeuralOperator class.

--- tests/operators/test_neuraloperator.py
+++ tests/operators/test_neuraloperator.py
@@ -1,6 +1,7 @@
 import pytest
-import torch
+from continuity.data.shape import TensorShape, DatasetShapes
 from continuity.benchmarks.sine import SineBenchmark
+from continuity.operators.integralkernel import NaiveIntegralKernel, NeuralNetworkKernel
 from continuity.operators import NeuralOperator
 from continuity.trainer import Trainer
 from continuity.operators.losses import MSELoss
@@ -11,17 +12,28 @@ def test_neuraloperator():
     # Data set
     dataset = SineBenchmark(n_train=1).train_dataset
 
+    shapes = dataset.shapes
+    obs = shapes.num_observations
+
+    latent_channels = 3
+    x1 = x2 = shapes.x
+    u1 = TensorShape(shapes.u.num, latent_channels)
+    u2 = TensorShape(shapes.u.num, latent_channels)
+
+    shapes = [
+        DatasetShapes(obs, x=shapes.x, u=shapes.u, y=x1, v=u1),
+        DatasetShapes(obs, x=x1, u=u1, y=x2, v=u2),
+        DatasetShapes(obs, x=x2, u=u2, y=shapes.y, v=shapes.v),
+    ]
+
     # Operator
-    operator = NeuralOperator(
-        shapes=dataset.shapes,
-        depth=1,
-        kernel_width=32,
-        kernel_depth=3,
-    )
+    layers = [
+        NaiveIntegralKernel(NeuralNetworkKernel(shapes[i], 32, 3)) for i in range(3)
+    ]
+    operator = NeuralOperator(dataset.shapes, layers)
 
     # Train
-    optimizer = torch.optim.Adam(operator.parameters(), lr=1e-2)
-    Trainer(operator, optimizer).fit(dataset, tol=1e-3)
+    Trainer(operator).fit(dataset, tol=1e-3)
 
     # Check solution
     x, u = dataset.x, dataset.u
