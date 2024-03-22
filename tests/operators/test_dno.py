7cb1c9e624940b228d37743a4815c393b8b72701
JakobEliasWagner <jakob@winter-wind.de>
2024-03-22 13:07:30 +0100

    add use fixture and utility function

--- tests/operators/test_dno.py
+++ tests/operators/test_dno.py
@@ -1,44 +1,18 @@
 import pytest
-import torch
 from typing import List
-from itertools import product
-from continuity.data import OperatorDataset
 from continuity.benchmarks.sine import SineBenchmark
 from continuity.trainer import Trainer
 from continuity.operators import DeepNeuralOperator
 from continuity.operators.losses import MSELoss
 
-
-@pytest.fixture(scope="module")
-def datasets() -> List[OperatorDataset]:
-    n_sensors = 3
-    n_evaluations = 5
-    n_observations = 7
-    x_dims = (1, 2)
-    u_dims = (1, 2)
-    y_dims = (1, 2)
-    v_dims = (1, 2)
-
-    datasets = []
-
-    for x_dim, u_dim, y_dim, v_dim in product(x_dims, u_dims, y_dims, v_dims):
-        x_samples = torch.rand(n_observations, n_sensors, x_dim)
-        u_samples = torch.rand(n_observations, n_sensors, u_dim)
-        y_samples = torch.rand(n_observations, n_evaluations, y_dim)
-        v_samples = torch.rand(n_observations, n_evaluations, v_dim)
-        datasets.append(
-            OperatorDataset(x=x_samples, u=u_samples, y=y_samples, v=v_samples)
-        )
-
-    return datasets
+from .util import get_shape_mismatches
 
 
 @pytest.fixture(scope="module")
-def dnos(datasets) -> List[DeepNeuralOperator]:
-    operators = []
-    for dataset in datasets:
-        operators.append(DeepNeuralOperator(dataset.shapes))
-    return operators
+def dnos(random_shape_operator_datasets) -> List[DeepNeuralOperator]:
+    return [
+        DeepNeuralOperator(dataset.shapes) for dataset in random_shape_operator_datasets
+    ]
 
 
 def test_can_initialize(dnos):
@@ -46,11 +20,8 @@ def test_can_initialize(dnos):
         assert isinstance(operator, DeepNeuralOperator)
 
 
-def test_output_shape_correct(dnos, datasets):
-    for operator, dataset in zip(dnos, datasets):
-        x, u, y, v = dataset[:9]  # batched sample
-        output = operator(x, u, y)
-        assert output.shape == v.shape
+def test_shapes(dnos, random_shape_operator_datasets):
+    assert get_shape_mismatches(dnos, random_shape_operator_datasets) == []
 
 
 @pytest.mark.slow
