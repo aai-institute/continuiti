c67e73d49b64189fdf4d642c026b331112318df0
Jakob <jakob@winter-wind.de>
2024-04-02 15:43:18 +0200

    add expose multihead attention

--- src/continuity/networks/__init__.py
+++ src/continuity/networks/__init__.py
@@ -7,5 +7,12 @@ Networks in Continuity.
 from .fully_connected import FullyConnected
 from .res_net import DeepResidualNetwork
 from .multi_head_attention import MultiHeadAttention
+from .function_encoder import FunctionEncoderLayer, FunctionEncoder
 
-__all__ = ["FullyConnected", "DeepResidualNetwork", "MultiHeadAttention"]
+__all__ = [
+    "FullyConnected",
+    "DeepResidualNetwork",
+    "MultiHeadAttention",
+    "FunctionEncoderLayer",
+    "FunctionEncoder",
+]
