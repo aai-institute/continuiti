fcd6656825aab697da6dd64e9323a5ea5ae30522
JakobEliasWagner <42122260+JakobEliasWagner@users.noreply.github.com>
2024-04-10 13:29:53 +0200

    Feature: Quantile Scaler (#112)
    * Add quantile scaler.
    
    Co-authored-by: Samuel Burbulla <s.burbulla@appliedai-institute.de>
    
    ---------
    
    Co-authored-by: Samuel Burbulla <s.burbulla@appliedai-institute.de>

--- src/continuity/transforms/__init__.py
+++ src/continuity/transforms/__init__.py
@@ -7,9 +7,6 @@ Data transformations in Continuity.
 from .transform import Transform
 from .compose import Compose
 from .scaling import Normalize
+from .quantile_scaler import QuantileScaler
 
-__all__ = [
-    "Transform",
-    "Compose",
-    "Normalize",
-]
+__all__ = ["Transform", "Compose", "Normalize", "QuantileScaler"]
