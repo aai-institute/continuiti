4a9fb4047bf4d275521e63688df4378975517068
Samuel Burbulla <s.burbulla@appliedai-institute.de>
2024-04-16 11:50:54 +0200

    Adopt cnn.

--- src/continuiti/operators/cnn.py
+++ src/continuiti/operators/cnn.py
@@ -5,7 +5,7 @@ The ConvolutionalNeuralNetwork (CNN) architecture.
 """
 
 import torch
-from typing import Optional, Tuple
+from typing import Optional
 from continuiti.operators import Operator
 from continuiti.operators.shape import OperatorShapes
 
@@ -20,8 +20,6 @@ class ConvolutionalNeuralNetwork(Operator):
         width: The number hidden channels.
         depth: The number of hidden layers.
         kernel_size: The size of the convolutional kernel.
-        grid_shape: x and y have to be sampled on a grid. If grid dimensions
-            are not specified, a grid with equal sizes is assumed.
         act: Activation function.
         device: Device.
     """
@@ -32,7 +30,6 @@ class ConvolutionalNeuralNetwork(Operator):
         width: int = 16,
         depth: int = 3,
         kernel_size: int = 3,
-        grid_shape: Optional[Tuple[int]] = None,
         act: Optional[torch.nn.Module] = None,
         device: Optional[torch.device] = None,
     ):
@@ -40,7 +37,6 @@ class ConvolutionalNeuralNetwork(Operator):
         super().__init__(shapes, device)
 
         self.act = torch.nn.Tanh() if act is None else act
-        self.grid_shape = grid_shape
         padding = kernel_size // 2
 
         assert shapes.x.dim in [1, 2, 3], "Only 1D, 2D, and 3D grids supported."
@@ -63,33 +59,20 @@ class ConvolutionalNeuralNetwork(Operator):
         """Forward pass through the operator.
 
         Performs the forward pass through the operator.
-        This assumes `x == y` and that `x` is a quadratic grid of positions.
 
         Args:
             x: Ignored.
-            u: Input function values of shape (batch_size, #sensors, u_dim).
+            u: Input function values of shape (batch_size, u_dim, num_sensors...).
             y: Ignored.
 
         Returns:
-            The output of the operator, of shape (batch_size, #evaluations, v_dim).
+            The output of the operator, of shape (batch_size, v_dim, num_evaluations...).
         """
-        # Transform input to (batch_size, u_dim, ux, uy, ...)
-        if self.grid_shape is not None:
-            u = u.reshape(-1, *self.grid_shape, self.shapes.u.dim)
-        else:
-            per_dim = int(self.shapes.u.num ** (1 / self.shapes.x.dim))
-            u = u.reshape([-1] + [per_dim] * self.shapes.x.dim + [self.shapes.u.dim])
-        u = u.swapaxes(1, -1)
-
         # Convolutional layers
         residual = u
         u = self.act(self.first_layer(u))
         for layer in self.hidden_layers:
             u = self.act(layer(u))
         u = self.last_layer(u) + residual
-
-        # Transform back to (batch_size, v_num, v_dim)
-        u = u.swapaxes(1, -1)
-        u = u.reshape(-1, self.shapes.v.num, self.shapes.v.dim)
 
         return u
