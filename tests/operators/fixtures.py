16c2730c01578f73580689923ce8e3ae031b4042
Samuel Burbulla <s.burbulla@appliedai-institute.de>
2024-04-16 11:58:27 +0200

    Extend shape test.

--- tests/operators/fixtures.py
+++ tests/operators/fixtures.py
@@ -37,4 +37,14 @@ def random_shape_operator_datasets() -> List[OperatorDataset]:
             OperatorDataset(x=x_samples, u=u_samples, y=y_samples, v=v_samples)
         )
 
+    # 2D grids
+    for x_dim, u_dim, y_dim, v_dim in product(x_dims, u_dims, y_dims, v_dims):
+        x_samples = torch.rand(n_observations, x_dim, n_sensors, n_sensors)
+        u_samples = torch.rand(n_observations, u_dim, n_sensors, n_sensors)
+        y_samples = torch.rand(n_observations, y_dim, n_evaluations, n_evaluations)
+        v_samples = torch.rand(n_observations, v_dim, n_evaluations, n_evaluations)
+        datasets.append(
+            OperatorDataset(x=x_samples, u=u_samples, y=y_samples, v=v_samples)
+        )
+
     return datasets
