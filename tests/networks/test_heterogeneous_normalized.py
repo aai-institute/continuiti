380c1786b9d794af2a20881b7123af358687e47a
JakobEliasWagner <jakob@winter-wind.de>
2024-06-17 13:52:23 +0200

    add unit tests

--- tests/networks/test_heterogeneous_normalized.py
+++ tests/networks/test_heterogeneous_normalized.py
@@ -19,6 +19,10 @@ def random_query_key_value_pair():
 
 
 class TestHeterogeneousNormalized:
+    def test_can_initialize(self):
+        _ = HeterogeneousNormalized()
+        assert True
+
     def test_shape_correct(self, random_query_key_value_pair):
         query, key, value = random_query_key_value_pair
 
@@ -28,3 +32,26 @@ class TestHeterogeneousNormalized:
         gt_out = scaled_dot_product_attention(query, key, value)
 
         assert out.shape == gt_out.shape
+
+    def test_gradient_flow(self, random_query_key_value_pair):
+        query, key, value = random_query_key_value_pair
+        query.requires_grad = True
+        key.requires_grad = True
+        value.requires_grad = True
+
+        attn = HeterogeneousNormalized()
+
+        out = attn(query, key, value)
+
+        out.sum().backward()
+
+        assert query.grad is not None, "Gradients not flowing to query"
+        assert key.grad is not None, "Gradients not flowing to key"
+        assert value.grad is not None, "Gradients not flowing to value"
+
+    def test_zero_input(self, random_query_key_value_pair):
+        query, key, value = random_query_key_value_pair
+        attn = HeterogeneousNormalized()
+
+        out = attn(torch.zeros(query.shape), key, value)
+        assert torch.allclose(torch.zeros(out.shape), out)
